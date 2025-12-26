8b29b610cc273bf8bb13cf59ae1fad3e998a0f7f
sbyang369 <139173281+sbyang369@users.noreply.github.com>
2025-12-26 15:04:57 -0500

    Update main.java
    Updated version with functions for efficiency

--- main.java
+++ main.java
@@ -4,10 +4,8 @@ public class Battleship {
         Scanner input = new Scanner(System.in);
         int [][] input1 = new int[5][2];
         int [][] input2 = new int[5][2];
-
         char [][] board1 = new char[5][5];
         char [][] board2 = new char[5][5];
-
         char [][] history1 = new char[5][5];
         char [][] history2 = new char[5][5];
 
@@ -15,142 +13,132 @@ public class Battleship {
 
         System.out.println("Welcome to Battleship!");
 
-        // player 1 input
-        for (int i=0;i<5;i++) {
-            boolean valid = false;
-            while (!valid) {
-                System.out.printf("Coordinate #%d ",i+1);
-                String line = input.nextLine();
-                String[] parts = line.split(" ");
-
-                if (parts.length != 2) {
-                    System.out.println("Invalid coordinates. Choose different coordinates0");
-                    continue;
-                }
-
-                try {
-                    int row = Integer.parseInt(parts[0]);
-                    int col = Integer.parseInt(parts[1]);
-
-                    if (row < 0 || row > 4 || col < 0 || col > 4) {
-                        System.out.println("Invalid coordinates. Choose different coordinates,");
-                        continue;
-                    }
-
-                    boolean duplicate = false;
-                    for (int x=0;x<i;x++) {
-                        if (row == input1[x][0]) {
-                            if (col == input1[x][1]) {
-                                duplicate = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (duplicate) {
-                        System.out.println("You already have a ship there. Choose different coordinates.");
-                        continue;
-                    }
-
-                    input1[i][0] = row;
-                    input1[i][1] = col;
-                    valid = true;
-
-                } catch (ArithmeticException e) {
-                    System.out.println("Invalid coordinates. Choose diffferent coordinates.");
-                }
-            }
-         }
-
-        for (int j=0;j<input1.length;j++) {
-            System.out.println(input1[j][0] + " " + input1[j][1]);
-        }
-
-        // print board
-        System.out.println();
-        for (int row=0;row<5;row++) {
-            for (int col=0;col<5;col++) {
-                board1[row][col] = '-';
-            }
-        }
-
-        for (int row=0;row<5;row++) {
-            for (int col=0;col<2;col++) {
-                board1[input1[row][0]][input1[row][1]] = '@';
-            }
-        }
-
+        // create p1 board
+        getUserInput(input, input1, "Player 1");
+        createBoard(board1,input1);
         printBattleShip(board1);
 
-        // player 2 input
-        System.out.println("PLAYER 2, ENTER YOUR SHIPS' COORDINATES.");
-        for (int i=0;i<5;i++) {
-            boolean valid = false;
-            while (!valid) {
-                System.out.printf("Coordinate #%d ",i+1);
-                String line = input.nextLine();
-                String[] parts = line.split(" ");
-
-                if (parts.length != 2) {
-                    System.out.println("Invalid coordinates. Choose different coordinates0");
-                    continue;
-                }
-
-                try {
-                    int row = Integer.parseInt(parts[0]);
-                    int col = Integer.parseInt(parts[1]);
-
-                    if (row < 0 || row > 4 || col < 0 || col > 4) {
-                        System.out.println("Invalid coordinates. Choose different coordinates,");
-                        continue;
-                    }
-
-                    boolean duplicate = false;
-                    for (int x=0;x<i;x++) {
-                        if (row == input2[x][0]) {
-                            if (col == input2[x][1]) {
-                                duplicate = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (duplicate) {
-                        System.out.println("You already have a ship there. Choose different coordinates.");
-                        continue;
-                    }
-
-                    input2[i][0] = row;
-                    input2[i][1] = col;
-                    valid = true;
-
-                } catch (ArithmeticException e) {
-                    System.out.println("Invalid coordinates. Choose diffferent coordinates.");
-                }
-            }
-        }
-
-        for (int j=0;j<input2.length;j++) {
-            System.out.println(input2[j][0] + " " + input2[j][1]);
-        }
-
-        // print board
-        System.out.println();
-        for (int row=0;row<5;row++) {
-            for (int col=0;col<5;col++) {
-                board2[row][col] = '-';
-            }
-        }
-
-        for (int row=0;row<5;row++) {
-            for (int col=0;col<2;col++) {
-                board2[input2[row][0]][input2[row][1]] = '@';
-            }
-        }
-
+        // create p2 board
+        getUserInput(input, input2, "Player 2");
+        createBoard(board2,input2);
         printBattleShip(board2);
 
         //history boards
+        createHistoryBoards(history1,history2);
+
+        space(5);
+
+        // Hit sequence
+        do {
+            //player 1's turn
+            int targetRow2;
+            int targetCol2;
+            System.out.println("Player 1, hit: ");
+            targetRow2 = input.nextInt();
+            targetCol2 = input.nextInt();
+
+            if (targetRow2 < 0 || targetRow2 > 4 || targetCol2 < 0 || targetCol2 > 4) {
+                System.out.println("Invalid coordinates. Choose different coordinates,");
+                continue;
+            }
+
+            turn("Player 1", "Player 2", board2, history1, targetRow2, targetCol2);
+
+            // player 2's turn
+            int targetRow1;
+            int targetCol1;
+            System.out.println("Player 2, hit: ");
+            targetRow1 = input.nextInt();
+            targetCol1 = input.nextInt();
+
+            if (targetRow1 < 0 || targetRow1 > 4 || targetCol1 < 0 || targetCol1 > 4) {
+                System.out.println("Invalid coordinates. Choose different coordinates,");
+                continue;
+            }
+
+            turn("Player 2", "Player 1", board1, history2, targetRow1, targetCol1);
+
+            //checker
+            int count1 = check(board1,input1, "Player 1");
+            if (count1 >= 5) {
+                System.out.println("PLAYER 2 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
+                running = false;
+            }
+
+            int count2= check(board2,input2,"Player 2");
+
+            if (count2 >= 5) {
+                System.out.println("PLAYER 1 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
+                running = false;
+            }
+
+        } while (running == true);
+
+    }
+
+    public static void createBoard(char[][] board,int[][] ships) {
+        for (int row=0;row<5;row++) {
+            for (int col=0;col<5;col++) {
+                board[row][col] = '-';
+            }
+        }
+
+        for (int row=0;row<5;row++) {
+            for (int col=0;col<2;col++) {
+                board[ships[row][0]][ships[row][1]] = '@';
+            }
+        }
+    }
+
+    public static void getUserInput(Scanner input, int[][] ships, String playerName) {
+        for (int i=0;i<5;i++) {
+            boolean valid = false;
+            while (!valid) {
+                System.out.printf("%s | Coordinate #%d:  ",playerName, i+1);
+                String line = input.nextLine();
+                String[] parts = line.split(" ");
+
+                if (parts.length != 2) {
+                    System.out.println("Invalid coordinates. Choose different coordinates0");
+                    continue;
+                }
+
+                try {
+                    int row = Integer.parseInt(parts[0]);
+                    int col = Integer.parseInt(parts[1]);
+
+                    if (row < 0 || row > 4 || col < 0 || col > 4) {
+                        System.out.println("Invalid coordinates. Choose different coordinates,");
+                        continue;
+                    }
+
+                    boolean duplicate = false;
+                    for (int x=0;x<i;x++) {
+                        if (row == ships[x][0]) {
+                            if (col == ships[x][1]) {
+                                duplicate = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (duplicate) {
+                        System.out.println("You already have a ship there. Choose different coordinates.");
+                        continue;
+                    }
+
+                    ships[i][0] = row;
+                    ships[i][1] = col;
+                    valid = true;
+
+                } catch (ArithmeticException e) {
+                    System.out.println("Invalid coordinates. Choose diffferent coordinates.");
+                }
+            }
+        }
+    }
+
+    public static void createHistoryBoards(char[][] history1,char[][] history2) {
         for (int row=0;row<5;row++) {
             for (int col=0;col<5;col++) {
                 history2[row][col] = '-';
@@ -163,97 +151,41 @@ public class Battleship {
             }
         }
 
-        for (int count =0;count<5;count++) {
+    }
+
+    public static void space(int num) {
+        for (int count =0;count<num;count++) {
             System.out.println();
         }
+    }
 
-        // Hit sequence
-        do {
+    public static void turn(String player, String opp, char[][] board, char[][] history, int targetRow, int targetCol) {
 
-            //repeat for player 1's turn
-            int targetRow2;
-            int targetCol2;
-            System.out.println("Player 1, hit: ");
-            targetRow2 = input.nextInt();
-            targetCol2 = input.nextInt();
+        // check if board has an 'O' / already hit. then update history + location
+        if (board[targetRow][targetCol] == 'O') {
+            System.out.println("You already fired on this spot. Choose different coordinates.");
+        } else if (board[targetRow][targetCol] == '-') {
+            System.out.printf("%s MISSED!%n", player);
+            history[targetRow][targetCol] = 'O';
+            board[targetRow][targetCol] = 'O';
+            printBattleShip(history);
+        } else {
+            System.out.printf("%s HIT %s's SHIP!%n", player, opp);
+            history[targetRow][targetCol] = 'X';
+            board[targetRow][targetCol] = 'X';
+            printBattleShip(history);
+        }
+    }
 
-            int targetCol;
-            if (targetRow2 < 0 || targetRow2 > 4 || targetCol2 < 0 || targetCol2 > 4) {
-                System.out.println("Invalid coordinates. Choose different coordinates,");
-                continue;
+    public static int check(char [][] board, int[][] input, String player) {
+        int count=0;
+        for (int row=0;row<5;row++) {
+            if (board[input[row][0]][input[row][1]] == 'X') {
+                count++;
+                System.out.printf("%s hits: %d %n", player,count);
             }
-
-            // check if board has an 'O' / already hit. then update history + location
-            if (board2[targetRow2][targetCol2] == 'O') {
-                System.out.println("You already fired on this spot. Choose different coordinates.");
-            } else if (board2[targetRow2][targetCol2] == '-') {
-                System.out.println("PLAYER 1 MISSED!");
-                history1[targetRow2][targetCol2] = 'O';
-                board2[targetRow2][targetCol2] = 'O';
-                printBattleShip(history1);
-            } else {
-                System.out.println("PLAYER 1 HIT PLAYER 2's SHIP!");
-                history1[targetRow2][targetCol2] = 'X';
-                board2[targetRow2][targetCol2] = 'X';
-                printBattleShip(history1);
-            }
-
-            // PLAYER 2'S TURN
-            int targetRow1;
-            int targetCol1;
-            System.out.println("Player 2, hit: ");
-            targetRow1 = input.nextInt();
-            targetCol1 = input.nextInt();
-
-            if (targetRow1 < 0 || targetRow1 > 4 || targetCol1 < 0 || targetCol1 > 4) {
-                System.out.println("Invalid coordinates. Choose different coordinates,");
-                continue;
-            }
-
-            // check if board has an 'O' / already hit. then update history + location
-            if (board1[targetRow1][targetCol1] == 'O') {
-                System.out.println("You already fired on this spot. Choose different coordinates.");
-            } else if (board1[targetRow1][targetCol1] == '-') {
-                System.out.println("PLAYER 2 MISSED!");
-                history2[targetRow1][targetCol1] = 'O';
-                board1[targetRow1][targetCol1] = 'O';
-                printBattleShip(history2);
-            } else {
-                System.out.println("PLAYER 2 HIT PLAYER 1's SHIP!");
-                history2[targetRow1][targetCol1] = 'X';
-                board1[targetRow1][targetCol1] = 'X';
-                printBattleShip(history2);
-            }
-
-            //include checker for if loop is done; if so, end
-            int count=0;
-            for (int row=0;row<5;row++) {
-                if (board1[input1[row][0]][input1[row][1]] == 'X') {
-                    count++;
-                    System.out.println("Player 2 hits: " + count);
-
-                }
-            }
-            if (count >= 5) {
-                System.out.println("PLAYER 2 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
-                running = false;
-            }
-
-            int count2=0;
-            for (int row=0;row<5;row++) {
-                if (board2[input2[row][0]][input2[row][1]] == 'X') {
-                    count2++;
-                    System.out.println("Player 1 hits: " + count2);
-                }
-            }
-
-            if (count2 >= 5) {
-                System.out.println("PLAYER 1 WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
-                running = false;
-            }
-
-        } while (running == true);
-
+        }
+        return count;
     }
 
     public static void printBattleShip(char[][] board) {
